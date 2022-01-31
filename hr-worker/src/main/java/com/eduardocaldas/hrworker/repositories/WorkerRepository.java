81098c411f3a77bfff66e55a208f1600c7f9f3ff
EduardoPPCaldas <eduardocaldas.dev@gmail.com>
2022-01-31 14:58:10 -0300

    hr-worker implementation

---
+++ hr-worker/src/main/java/com/eduardocaldas/hrworker/repositories/WorkerRepository.java
@@ -0,0 +1,11 @@
+package com.eduardocaldas.hrworker.repositories;
+
+import com.eduardocaldas.hrworker.entities.Worker;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface WorkerRepository extends JpaRepository<Worker, Long> {
+    
+}
